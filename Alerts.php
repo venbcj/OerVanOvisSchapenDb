b20c0af2e7bb141c93baf680d9ba16e416a9de4d
venbcj <bas.vandeven@telfort.nl>
2025-09-02 16:26:38 +0200

    Add files via upload

--- Alerts.php
+++ Alerts.php
@@ -1,23 +1,22 @@
 <?php 
 $versie = '20-12-2020'; /* Pagina gemaakt */
+$versie = '31-12-2024'; /* <TD width = 960 height = 400 align = "center" > gewijzigd naar <TD align = "center"> 31-12-24 Include "login.php"; voor Include "header.php" gezet */
 
 session_start(); ?>
+<!DOCTYPE html>
 <html>
 <head>
 <title>Menu</title>
 </head>
 <body>
 
-<center>
 <?php
 $titel = 'Readeralerts';
-$subtitel = '';
-Include "header.php"; ?>
-
-<td width = 960 height = 400 align = "center">
-<?php 
 $file = "Alerts.php";
-Include "login.php";
+Include "login.php"; ?>
+
+		<TD align = "center">
+<?php 
 if (isset($_SESSION["U1"]) && isset($_SESSION["W1"]) && isset($_SESSION["I1"])) { 
 ?>
 <img src= "OER_van_OVIS.jpg" width= 650 height= 240 valign = "center"/>
@@ -27,7 +26,6 @@ Include "menuAlerts.php"; } ?>
 </tr>
 
 </table>
-</center>
 
 </body>
 </html>
