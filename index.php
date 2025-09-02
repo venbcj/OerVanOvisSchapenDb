6ad35f9a06b2941ec7497358ea6c915853f135f5
venbcj <bas.vandeven@telfort.nl>
2025-09-02 17:34:37 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,27 +1,27 @@
 <?php /* 29-8-2018 titel.php verwijderd. Zit in header.php samen met Style.css 
 23-5-2020 logo aangepast 
 11-7-2020  $file = "index.php"; gewijzigd naar $file = "Home.php"; */
-session_start();
-// destroy the session 
-session_destroy();   ?>
+$versie = '26-12-2024'; /* <TD width = 1390 height = 400 align = "center"> gewijzigd naar <TD align = "center">  */
+	session_start();
+ // destroy the session 
+ session_destroy(); ?>
+<!DOCTYPE html>
 <html>
 <head>
 <title>Home</title>
 </head>
 <body>
 
-<center>
 <?php 
 //Include "titel.php"; ?> 
 <!-- <table><tr align = center style = "font-size : 30px ";><td>OER van OVIS</td> </tr>
 <tr align = center><td><sup style = "font-size : 18px "; >Optimalisering En Rendementverbetering van het Schaap</sup></td></tr></table>-->
 <?php 
-$titel = 'OER van OVIS';
-$subtitel = 'Optimalisering En Rendementverbetering van het Schaap';
-Include "header.php"; 
+$titel = '';
+Include "header_logout.php"; 
  ?>
 
-<TD width = 1390 height = 400 align = "center">
+<TD align = "center">
 	<br>
 	<br>
 	<br>
