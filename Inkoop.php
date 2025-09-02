5c1fac7418c89603d74e2503734a02fbe95a4e54
venbcj <bas.vandeven@telfort.nl>
2025-09-02 16:48:29 +0200

    Add files via upload

--- Inkoop.php
+++ Inkoop.php
@@ -3,24 +3,23 @@ $versie = '8-3-2015'; /*Login toegevoegd*/
 $versie = '14-11-2015'; /*naamwijziging van Menu Inkoop naar Menu Voorraadbeheer*/
 $versie = '28-9-2018'; /* titel.php verwijderd. Zit in header.php samen met Style.css */
 $versie = '23-5-2020'; /* jpg aangepast */
+$versie = '26-12-2024'; /* <td width = 960 height = 400 align = "center"> gewijzigd naar <TD align = "center"> 31-12-24 Include "login.php"; voor Include "header.php" gezet */
 
 session_start(); ?>
+<!DOCTYPE html>
 <html>
 <head>
 <title>Menu</title>
 </head>
 <body>
 
-<center>
 <?php 
 $titel = 'Menu Voorraadbeheer';
-$subtitel = ''; 
-Include "header.php"; ?>
-
-<td width = 960 height = 400 align = "center">
-<?php
 $file = "Inkoop.php";
-Include "login.php";
+Include "login.php"; ?>
+
+			<TD align = "center">
+<?php
 if (isset($_SESSION["U1"]) && isset($_SESSION["W1"]) && isset($_SESSION["I1"])) { ?>
 
 <img src="OER_van_OVIS.jpg" width= 650 height= 240 valign = "center"/>
@@ -31,7 +30,6 @@ Include "menuInkoop.php"; } ?>
 </tr>
 
 </table>
-</center>
 
 </body>
 </html>
