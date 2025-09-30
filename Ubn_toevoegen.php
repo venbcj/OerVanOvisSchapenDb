4c9f2066f94d6b2a908e9cd521a244ce44585b38
Bas Cost Budde <bas@clickhere.nl>
2025-09-30 17:01:05 +0200

    verplaatst queries uit ubn_toevoegen naar gateways; schoont html op

--- Ubn_toevoegen.php
+++ Ubn_toevoegen.php
@@ -24,30 +24,22 @@ if (Auth::is_logged_in()) {
 
 if (isset($_POST['knpSave_'])) { include "save_ubn.php"; }
 
-if (isset($_POST['knpInsert_']))
-{
+if (isset($_POST['knpInsert_'])) {
     $new_ubn = $_POST['insUbn_'];
     $new_adres = $_POST['insAdres_'];
     $new_plaats = $_POST['insPlaats_'];
+
+    $ubn_gateway = new UbnGateway($db);
 // Zoek naar ubn op duplicaten
-$controle = mysqli_query($db,"
-SELECT count(ubn) aantal
-FROM tblUbn
-WHERE lidId = '".mysqli_real_escape_string($db,$lidId)."' and ubn = '".mysqli_real_escape_string($db,$new_ubn)."'
-") or die (mysqli_error($db));
-                while ($row = mysqli_fetch_assoc($controle))
-                {
-                    $dubbel = $row['aantal'];
-                } // Einde Zoek naar ubn op duplicaten
+    $dubbel = $ubn_gateway->exists_for_lid($new_ubn, $lidId);
     
-    if (empty($_POST['insUbn_']))                 { $fout = "U heeft geen ubn ingevoerd."; }    
-    else if (isset($dubbel) && $dubbel > 0)         { $fout = "Dit ubn bestaat al.";    $new_ubn = '';    }    
-    else 
-    {
-$ubn_toevoegen = "
-  INSERT INTO tblUbn SET lidId = '".mysqli_real_escape_string($db,$lidId)."', ubn = '".mysqli_real_escape_string($db,$new_ubn)."', adres = ".db_null_input($new_adres).", plaats = ".db_null_input($new_plaats) ;
-        
-                /*echo $ubn_toevoegen; */ mysqli_query($db,$ubn_toevoegen) or die (mysqli_error($db));
+    if (empty($_POST['insUbn_']))                 {
+        $fout = "U heeft geen ubn ingevoerd."; 
+    }    else if ($dubbel)         {
+        $fout = "Dit ubn bestaat al.";
+        $new_ubn = '';
+    }    else {
+        $ubn_gateway->insert_with_plaats($lidId, $new_ubn, $new_adres, $new_plaats);
     }
 } ?>
 
@@ -62,10 +54,10 @@ $ubn_toevoegen = "
  <td> <b> Woonplaats : </b> </td>
 </tr>
 <tr>
- <td> <input type= "text" name= "insUbn_"      size="8"     value = <?php if(isset($new_ubn)) { echo $new_ubn; }; ?> > </td>
- <td> <input type= "text" name= "insAdres_"                       value = <?php if(isset($new_adres)) { echo " \"$new_adres\" "; }; ?> > </td>
- <td> <input type= "text" name= "insPlaats_" size="12" value = <?php if(isset($new_plaats)) { echo " \"$new_plaats\" "; }; ?> > </td>
- <td> <input type = "submit" name= "knpInsert_" value = "Toevoegen" > </td>
+ <td> <input type="text" name="insUbn_" size="8" value="<?php if(isset($new_ubn)) { echo $new_ubn; }; ?>" > </td>
+ <td> <input type="text" name="insAdres_" value="<?php if(isset($new_adres)) { echo " \"$new_adres\" "; }; ?>" > </td>
+ <td> <input type="text" name="insPlaats_" size="12" value="<?php if(isset($new_plaats)) { echo " \"$new_plaats\" "; }; ?>" > </td>
+ <td> <input type="submit" name="knpInsert_" value="Toevoegen" > </td>
 </tr>
 </table>
 
@@ -78,65 +70,51 @@ $ubn_toevoegen = "
  <td align="center"> ver-<br>wijder <hr></td>
  <td align="center" valign="bottom"> Adres <hr></td>
  <td align="center" valign="bottom"> Woonplaats <hr></td>
- <td align="center" valign="bottom" width="100"> <input type = "submit" name= "knpSave_" value = "Opslaan" style = "font-size:12px;"> <hr></td>
+ <td align="center" valign="bottom" width="100"> <input type="submit" name="knpSave_" value="Opslaan" style="font-size:12px;"> <hr></td>
  <td ></td>    
 </tr>
 
-
 <?php
 // START LOOP    
-$zoek_ubn = mysqli_query($db,"
-SELECT ubnId, ubn, adres, plaats, actief
-FROM tblUbn
-WHERE lidId = '".mysqli_real_escape_string($db,$lidId)."'
-ORDER BY actief desc, ubn
-") or die (mysqli_error($db));
-
-    while($row = mysqli_fetch_assoc($zoek_ubn))
-    {
+$zoek_ubn = $ubn_gateway->zoek_met_plaats($lidId);
+    while($row = mysqli_fetch_assoc($zoek_ubn)) {
       $Id = $row['ubnId'];
       $ubnnr = $row['ubn'];
       $adres = $row['adres'];
       $woonplaats = $row['plaats'];
       $actief = $row['actief'];
-
-
-$zoek_db_tabelrelaties =    mysqli_query($db,"
-SELECT u.ubnId
-FROM tblUbn u
- left join tblStal st on (st.ubnId = u.ubnId)
-WHERE u.ubnId = '".mysqli_real_escape_string($db,$Id)."' and isnull(st.stalId)
-") or die (mysqli_error($db));
-
-    while($zdr = mysqli_fetch_assoc($zoek_db_tabelrelaties))
-    { $dbRelatie = $zdr['ubnId']; }  
-
+      $dbRelatie = $ubn_gateway->zoek_relatie($row['ubnId']);
 
 If(!isset($_POST['txtAdres'])) { $txtAdres = $adres; } else { $txtAdres = $_POST['txtAdres']; } 
 If(!isset($_POST['txtPlaats'])) { $txtPlaats = $woonplaats; } else { $txtPlaats = $_POST['txtPlaats']; } 
 
 if($actief == 0) { $color = '#E2E2E2'; } else { $color = 'black'; }
 ?>
-
-
 <tr>
- <td style = "color : <?php echo $color; ?> ;" > <?php echo $ubnnr; ?> </td>
+ <td style="color : <?php echo $color; ?> ;" > <?php echo $ubnnr; ?> </td>
  <td align="center"> 
-    <input type = "checkbox" name = <?php echo "chbActief_$Id"; ?> id="c1" value= 1 <?php echo $actief == 1 ? 'checked' : ''; ?>         title = "Is ubn te gebruiken ja/nee ?">
+    <input type="checkbox" name="chbActief_<?php echo $Id; ?>" id="c1" value="1"<?php echo $actief == 1 ? ' checked' : ''; ?>
+title="Is ubn te gebruiken ja/nee ?">
  </td>
  <td align="center">
      <?php if(isset($dbRelatie)) { ?>
-     <input type="checkbox" name= <?php echo "chbDel_$Id"; ?> >
- <?php } unset($dbRelatie); ?>
+     <input type="checkbox" name="chbDel_<?php echo $Id; ?> >"
+ <?php } ?>
  </td>
- <td style = "color : <?php echo $color; ?> ;" >
-<?php if($actief == 0) { echo $txtAdres; } else { ?> 
-     <input type="text" name= <?php echo "txtAdres_$Id"; ?> value = <?php echo " \"$txtAdres\" "; ?> >
+ <td style="color: <?php echo $color; ?> ;" >
+<?php
+if($actief == 0) {
+    echo $txtAdres;
+} else { ?> 
+     <input type="text" name="txtAdres_<?php echo $Id; ?>" value="<?php echo $txtAdres; ?>">
 <?php } ?>
  </td>
- <td style = "color : <?php echo $color; ?> ;" >
-<?php if($actief == 0) { echo $txtPlaats; } else { ?> 
-     <input type="text" name= <?php echo "txtPlaats_$Id"; ?> size = 12 value = <?php echo " \"$txtPlaats\" "; ?> >
+ <td style="color: <?php echo $color; ?> ;" >
+<?php
+    if($actief == 0) {
+        echo $txtPlaats;
+    } else { ?> 
+     <input type="text" name="txtPlaats_<?php echo $Id; ?>" size="12" value="<?php echo $txtPlaats; ?>">
 <?php } ?>
  </td>
 </tr>
@@ -147,8 +125,6 @@ if($actief == 0) { $color = '#E2E2E2'; } else { $color = 'black'; }
 </td></tr></table>
 
 </form>
-
-
 
     </TD>
 <?php
--- gateways/UbnGateway.php
+++ gateways/UbnGateway.php
@@ -4,12 +4,58 @@ class UbnGateway extends Gateway {
 
     public function exists($ubn) {
         // zou ook met count() kunnen
-        $vw = mysqli_query($this->db, "SELECT ubn FROM tblUbn WHERE ubn = '".mysqli_real_escape_string($this->db, $ubn)."' ;") or Logger::error(mysqli_error($this->db));
-        return (mysqli_num_rows($vw) != 0);
+        return 0 < $this->run_query(<<<SQL
+SELECT ubn FROM tblUbn WHERE ubn = :ubn
+SQL
+        , [[':ubn', $ubn]])->num_rows;
+    }
+
+    public function exists_for_lid($ubn, $lidId) {
+        return 0 < $this->first_field(<<<SQL
+SELECT count(*) FROM tblUbn WHERE lidId = :lidId and ubn = :ubn
+SQL
+        , [[':lidId', $lidId, self::INT], [':ubn', $ubn, self::TXT]]);
     }
 
     public function insert($lidId, $ubn) {
-        mysqli_query($this->db, "INSERT INTO tblUbn SET lidId = '".mysqli_real_escape_string($this->db, $lidId)."', ubn = '".mysqli_real_escape_string($this->db, $ubn)."' ");
+        $this->run_query(<<<SQL
+INSERT INTO tblUbn SET lidId = :lidId, ubn = :ubn
+SQL
+        , [[':lidId', $lidId, self::INT], [':ubn', $ubn]]);
+    }
+
+    public function insert_with_plaats($lidId, $new_ubn, $new_adres, $new_plaats) {
+        $this->run_query(<<<SQL
+INSERT INTO tblUbn SET lidId = :lidId, ubn = :ubn,
+ adres = :adres, plaats = :plaats
+SQL
+        , [
+            [':lidId', $lidId, self::INT],
+            [':ubn', $ubn], // default type is txt
+            [':adres', $new_adres],
+            [':plaats', $new_plaats],
+        ]);
+    }
+
+    public function zoek_met_plaats($lidId) {
+        return $this->run_query(<<<SQL
+SELECT ubnId, ubn, adres, plaats, actief
+FROM tblUbn
+WHERE lidId = :lidId
+ORDER BY actief desc, ubn
+SQL
+        , [[':lidId', $lidId, self::INT]]);
+    }
+
+    public function zoek_relatie($ubnId) {
+        return $this->first_field(<<<SQL
+SELECT u.ubnId
+FROM tblUbn u
+ left join tblStal st on (st.ubnId = u.ubnId)
+WHERE u.ubnId = :ubnId
+ and isnull(st.stalId)
+SQL
+        , [[':ubnId', $ubnId, self::INT]]);
     }
 
 }
