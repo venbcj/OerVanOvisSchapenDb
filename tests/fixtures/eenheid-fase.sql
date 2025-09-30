0ea6b7f71d9e1868acdedb2f5ad93d440576900d
Bas Cost Budde <bas@clickhere.nl>
2025-09-30 12:35:36 +0200

    herstelt achtergebleven unit-test nadat de stamtabellen zijn ingelezen

--- tests/fixtures/eenheid-fase.sql
+++ tests/fixtures/eenheid-fase.sql
@@ -5,7 +5,7 @@ delete from tblHistorie;
 delete from tblStal;
 delete from tblSchaap;
 
-insert into tblArtikel(artId, enhuId, soort, naam) values(1, 1, 1, 'a');
+insert into tblArtikel(artId, enhuId, soort, naam) values(1, 3, 1, 'a');
 insert into tblInkoop(artId, inkId, inkat, enhuId, prijs) values(1, 1, 1, 1, 1);
 insert into tblNuttig(inkId, hisId) values(1, 1);
 insert into tblHistorie(hisId, stalId, datum) values(1, 1, '1970-01-01');
