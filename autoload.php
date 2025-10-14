644fe8bbae478eb7f82fe8267321723b13f35489
Bas Cost Budde <bas@clickhere.nl>
2025-10-14 15:47:42 +0200

    voegt error-settings toe aan autoload

--- autoload.php
+++ autoload.php
@@ -1,5 +1,8 @@
 <?php
 
+ini_set('display_errors', true);
+error_reporting(E_ALL);
+
 # include-path voorlopig maar even hier
 $app_folders = ['classes', 'templates', 'fpdf', 'gateways'];
 set_include_path(implode(':', array_merge(explode(':', get_include_path()), $app_folders)));
