7b3d894faca2f49945f6928f0c747a60e7e0093d
Bas Cost Budde <bas@heuveltop.nl>
2025-11-29 13:21:48 +0100

    test overige functies in artikelnuttigen

--- func_artikelnuttigen.php
+++ func_artikelnuttigen.php
@@ -3,45 +3,38 @@
 /*5-9-2021 : functie inlezen_voer gemaakt 22-9-2021 functie inlezen_pil gemaakt */
 
 function volgende_inkoop_voer($datb, $artikel) {
-
     $zoek_volgende_inkoopdatum = mysqli_query($datb, "
   SELECT min(dmink) dmink
   FROM tblInkoop i
    left join tblVoeding v on (i.inkId = v.inkId) 
   WHERE artId = '" . mysqli_real_escape_string($datb, $artikel) . "' and isnull(v.inkId)
 ");
-
     while ($v_inkdm = mysqli_fetch_assoc($zoek_volgende_inkoopdatum)) {
         $dmink = $v_inkdm['dmink'];
     }
-
     $zoek_volgende_inkId = mysqli_query($datb, "
   SELECT min(i.inkId) inkId
   FROM tblInkoop i
    left join tblVoeding v on (i.inkId = v.inkId)
   WHERE artId = '" . mysqli_real_escape_string($datb, $artikel) . "' and dmink = '" . mysqli_real_escape_string($datb, $dmink) . "' and isnull(v.inkId)
 ");
-
     while ($v_inkId = mysqli_fetch_assoc($zoek_volgende_inkId)) {
         $new_inkId = $v_inkId['inkId'];
     }
-
     $zoek_inkoophoeveelheid = mysqli_query($datb, "
   SELECT i.inkId, i.inkat, a.stdat
   FROM tblInkoop i
    join tblArtikel a on (i.artId = a.artId)
   WHERE inkId = '" . mysqli_real_escape_string($datb, $new_inkId) . "'
 ");
-
+$inkoop = null;
     while ($ih = mysqli_fetch_assoc($zoek_inkoophoeveelheid)) {
         $inkoop = array($ih['inkId'], $ih['inkat'], $ih['stdat']);
     }
-
     return $inkoop;
 }
 
 function zoek_voorraad_oudste_inkoop_voer($datb, $artikel) {
-
     $zoek_inkId_en_resterende_voorraad_van_laatst_aangesproken_voorraad = mysqli_query($datb, "
 SELECT i.inkId, i.inkat - coalesce(n.nutat,0) vrdat, a.stdat
 FROM tblArtikel a
@@ -51,46 +44,34 @@ FROM tblArtikel a
     FROM tblVoeding 
     GROUP BY inkId
  ) n on (i.inkId = n.inkId)
-WHERE i.artId = '" . mysqli_real_escape_string($datb, $artikel) . "' and i.inkat > (i.inkat - coalesce(n.nutat,0)) and (i.inkat - coalesce(n.nutat,0)) > 0
+WHERE i.artId = '" . mysqli_real_escape_string($datb, $artikel) . "'
+ and i.inkat > (i.inkat - coalesce(n.nutat,0))
+ and (i.inkat - coalesce(n.nutat,0)) > 0
 ");
     while ($i_vrd = mysqli_fetch_assoc($zoek_inkId_en_resterende_voorraad_van_laatst_aangesproken_voorraad)) {
         $inkoop = array($i_vrd['inkId'], $i_vrd['vrdat'], $i_vrd['stdat']);
     }
-
     if (!isset($inkoop[0])) {
         $inkoop = volgende_inkoop_voer($datb, $artikel);
     }
-
     return $inkoop;
 }
 
 function inlezen_voer($datb, $artid, $rest_toedat, $toediendatum, $periode_id, $readerid) {
-
     $ink_voorraad = zoek_voorraad_oudste_inkoop_voer($datb, $artid);
-
     $inkId = $ink_voorraad[0];
     $rest_ink_vrd = $ink_voorraad[1];
     $stdat = $ink_voorraad[2];
-
-
     if ($rest_toedat > $rest_ink_vrd) {
         $inlezen_voer = "INSERT INTO tblVoeding SET periId = '" . mysqli_real_escape_string($datb, $periode_id) . "', inkId = '" . mysqli_real_escape_string($datb, $inkId) . "', nutat = '" . mysqli_real_escape_string($datb, $rest_ink_vrd) . "', stdat = '" . mysqli_real_escape_string($datb, $stdat) . "', datum = " . db_null_input($toediendatum) . ", readerId = " . db_null_input($readerid) . " ";
-
         mysqli_query($datb, $inlezen_voer);
-
-
         $rest_toedat = $rest_toedat - $rest_ink_vrd;
-
-
         inlezen_voer($datb, $artid, $rest_toedat, $toediendatum, $periode_id, $readerid);
     } else {
         $inlezen_voer = "INSERT INTO tblVoeding SET periId = '" . mysqli_real_escape_string($datb, $periode_id) . "', inkId = '" . mysqli_real_escape_string($datb, $inkId) . "', nutat = '" . mysqli_real_escape_string($datb, $rest_toedat) . "', stdat = '" . mysqli_real_escape_string($datb, $stdat) . "', datum = " . db_null_input($toediendatum) . ", readerId = " . db_null_input($readerid) . " ";
-
         mysqli_query($datb, $inlezen_voer);
     }
 }
-
-
 
 function volgende_inkoop_pil($artikel): array {
     $inkoop_gateway = new InkoopGateway();
--- tests/unit/ArtikelnuttigenTest.php
+++ tests/unit/ArtikelnuttigenTest.php
@@ -11,7 +11,7 @@ class ArtikelnuttigenTest extends IntegrationCase {
 
     public function test_inlezen_pil() {
         // GIVEN
-        $this->setArtikelFixture();
+        $this->setArtikelPilFixture();
         $this->expectNewRecordsInTables([
             'tblNuttig' => 1,
         ]);
@@ -28,7 +28,7 @@ class ArtikelnuttigenTest extends IntegrationCase {
 
     public function test_inlezen_pil_meerDan() {
         // GIVEN
-        $this->setArtikelFixture();
+        $this->setArtikelPilFixture();
         // als je onvoldoende inkoopt, klapt de test eruit. 3 is genoeg. Nee 4. Waarom?
         $this->runSQL("INSERT INTO tblInkoop(inkId, dmink, artId, inkat, enhuId, prijs)
             VALUES(2, '2012-01-01', " . self::ARTID . ", 4, 1, 1)");
@@ -38,7 +38,7 @@ class ArtikelnuttigenTest extends IntegrationCase {
         // WHEN
         $hisid = 0;
         $artid = self::ARTID;
-        $rest_toedat = 1;
+        $rest_toedat = 1; // dat is "meer dan" 0 in de basis-test
         $toediendatum = '';
         $reduid = 0;
         inlezen_pil($this->db, $hisid, $artid, $rest_toedat, $toediendatum, $reduid);
@@ -47,14 +47,14 @@ class ArtikelnuttigenTest extends IntegrationCase {
     }
 
     public function test_volgende_inkoop_pil_throws_exception_when_insufficient_voorraad() {
-        $this->setArtikelFixture();
+        $this->setArtikelPilFixture();
         $artid = self::ARTID;
         $this->expectException(Exception::class);
         $actual = volgende_inkoop_pil($artid);
     }
 
     public function test_volgende_inkoop_pil() {
-        $this->setArtikelFixture();
+        $this->setArtikelPilFixture();
         $this->runSQL("INSERT INTO tblInkoop(inkId, dmink, artId, inkat, enhuId, prijs) VALUES(2, '2012-01-01', " . self::ARTID . ", 1, 1, 1)");
         $artid = self::ARTID;
         $actual = volgende_inkoop_pil($artid);
@@ -67,7 +67,7 @@ class ArtikelnuttigenTest extends IntegrationCase {
         // zet de database goed
         // Voor het eerste scenario vind ik het belangrijk dat we niet verder afdalen
         //   in volgende_inkoop_pil.
-        $this->setArtikelFixture();
+        $this->setArtikelPilFixture();
         // WHEN
         // voer de methode uit
         $artid = self::ARTID;
@@ -78,18 +78,56 @@ class ArtikelnuttigenTest extends IntegrationCase {
         $this->assertEquals($expected, $actual);
     }
 
+    public function test_inlezen_voer() {
+        $this->setArtikelVoerFixture();
+        $this->expectNewRecordsInTables([
+            'tblVoeding' => 1,
+        ]);
+        $artid = self::ARTID;
+        $rest_toedat = 0;
+        $toediendatum = '';
+        $periode_id = 0;
+        $readerid = 0;
+        inlezen_voer($this->db, $artid, $rest_toedat, $toediendatum, $periode_id, $readerid);
+        $this->assertTablesGrew();
+    }
+
+    public function test_inlezen_voer_meerDan() {
+        $this->setArtikelVoerFixture();
+        $this->runSQL("INSERT INTO tblInkoop(inkId, dmink, artId, inkat, enhuId, prijs)
+            VALUES(2, '2012-01-01', " . self::ARTID . ", 4, 1, 1)");
+        $this->expectNewRecordsInTables([
+            'tblVoeding' => 2,
+        ]);
+        $artid = self::ARTID;
+        $rest_toedat = 2;
+        $toediendatum = '';
+        $periode_id = 0;
+        $readerid = 0;
+        inlezen_voer($this->db, $artid, $rest_toedat, $toediendatum, $periode_id, $readerid);
+        $this->assertTablesGrew();
+    }
+
     # TODO: scenarios voor:
-    # inlezen_voer
     # volgende_inkoop_voer
     # zoek_voorraad_oudste_inkoop_voer
 
-    private function setArtikelFixture() {
+    private function setArtikelPilFixture() {
         $this->runSQL("TRUNCATE tblInkoop");
-        $this->runSQL("INSERT INTO tblInkoop(inkId, artId, inkat, enhuId, prijs) VALUES(1, " . self::ARTID . ", 2, 1, 1)");
         $this->runSQL("TRUNCATE tblNuttig");
-        $this->runSQL("INSERT INTO tblNuttig(inkId, nutat, stdat) VALUES(1, 1, 1)");
         $this->runSQL("TRUNCATE tblArtikel");
         $this->runSQL("INSERT INTO tblArtikel(artId, naam, stdat, soort) VALUES(" . self::ARTID . ", 'test', 4, 0)");
+        $this->runSQL("INSERT INTO tblInkoop(inkId, artId, inkat, enhuId, prijs) VALUES(1, " . self::ARTID . ", 2, 1, 1)");
+        $this->runSQL("INSERT INTO tblNuttig(inkId, nutat, stdat) VALUES(1, 1, 1)");
+    }
+
+    private function setArtikelVoerFixture() {
+        $this->runSQL("TRUNCATE tblArtikel");
+        $this->runSQL("TRUNCATE tblInkoop");
+        $this->runSQL("TRUNCATE tblVoeding");
+        $this->runSQL("INSERT INTO tblArtikel(artId, naam, stdat, soort) VALUES(" . self::ARTID . ", 'test', 4, 0)");
+        $this->runSQL("INSERT INTO tblInkoop(inkId, artId, inkat, enhuId, prijs) VALUES(1, " . self::ARTID . ", 2, 1, 1)");
+        $this->runSQL("INSERT INTO tblVoeding(inkId, nutat, stdat) VALUES(1, 1, 1)");
     }
 
 }
