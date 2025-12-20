8c16e136d7f4e1869236bbf30219f8e21e29f7be
Bas van de Ven <bvdb73@telfort.nl>
2025-12-20 16:05:52 +0100

    Maakt functies op een windows machine ook mogelijk

--- autoload.php
+++ autoload.php
@@ -19,7 +19,7 @@ $app_folders = [
 set_include_path(implode(PATH_SEPARATOR, $app_folders));
 
 spl_autoload_register(function ($class) {
-    foreach (explode(':', get_include_path()) as $prefix) {
+    foreach (explode(PATH_SEPARATOR, get_include_path()) as $prefix) {
         foreach (explode(' ', ' .class .trait') as $type) {
             $file = "$class$type.php";
             if (file_exists("$prefix/$file")) {
@@ -29,5 +29,5 @@ spl_autoload_register(function ($class) {
         }
     }
 });
-//Session::set_instance(new Session());
-//Response::setProduction();
+
+//trigger_error("Autoloader: Class '$class' not found in include path.", E_USER_WARNING);
