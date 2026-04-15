bc5eafbcccf3e147efb4ec9edfc92ef5ee7a1454
Bas van de Ven <bvdb73@telfort.nl>
2026-04-15 12:21:05 +0200

    maakt invoer eigen redenen mogelijk.

---
+++ templates/validate-reden.js.php
@@ -0,0 +1,7 @@
+    <script>
+function verplicht() {
+var eigenReden = document.getElementById("txtReden"); var eigenReden_v = eigenReden.value;
+
+     if(eigenReden_v.length > 50) eigenReden.focus()     + alert("De Reden mag max 50 karakters zijn.");
+}
+</script>
