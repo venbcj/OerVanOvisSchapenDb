7f426de00bbeabb06196bb464acc505d554463a8
Bas van de Ven <bvdb73@telfort.nl>
2026-09-06 11:55:54 +0200

    voegt ontbrekende html uit Main toe.

--- InsMedicijn.php
+++ InsMedicijn.php
@@ -188,7 +188,151 @@ else if (isset($_POST['knpVervers_'])) { $cbKies = $_POST["chbkies_$Id"];  $cbDe
     <input type = hidden size = 1 name = <?php echo "chbDel_$Id"; ?> value = 0 >
     <input type = checkbox class="delete" name = <?php echo "chbDel_$Id"; ?> value = 1 <?php if(isset($cbDel)) { echo $cbDel == 1 ? 'checked' : ''; } ?> >
  </td>
+ <td>
+    <input type = "text" size = 9 style = "font-size : 11px;" name = <?php echo "txtDatum_$Id"; ?> value = <?php echo $dag; ?> >
+ </td>
+
+<?php if(!empty($fase)) { ?> <td> <?php echo $levnr;} else { ?> <td style = "color : red"> <?php echo $levnr;} ?>
+ </td>
+
+
+<?php 
+// Declaratie MEDICIJN (De medicijnen uit voorraad)
+$zoek_artId_op_voorraad = mysqli_query($db," 
+SELECT a.artId, a.naam, a.stdat, e.eenheid, sum(i.inkat-coalesce(n.vbrat,0)) vrdat
+FROM tblEenheid e
+ join tblEenheiduser eu on (e.eenhId = eu.eenhId)
+ join tblInkoop i on (i.enhuId = eu.enhuId)
+ join tblArtikel a on (i.artId = a.artId)
+ left join (
+    SELECT n.inkId, sum(n.nutat*n.stdat) vbrat
+    FROM tblNuttig n
+    GROUP BY n.inkId
+ ) n on (i.inkId = n.inkId)
+WHERE eu.lidId = '".mysqli_real_escape_string($db,$lidId)."' and i.inkat-coalesce(n.vbrat,0) > 0 and a.soort = 'pil'
+GROUP BY a.artId, a.naam, a.stdat, e.eenheid
+ORDER BY a.naam
+") or die (mysqli_error($db));
+
+$index = 0;
+while ($pil = mysqli_fetch_array($zoek_artId_op_voorraad))
+{
+   $pilId[$index] = $pil['artId'];
+   $pilln[$index] = $pil['naam'];
+   $pilRaak[$index] = $pil['artId'];
+   $index++;
+}
+unset($index);
+// EINDE Declaratie MEDICIJN ?> 
+
+
+ <td style = "font-size : 9px;" >
+<!-- KZLMEDICIJN -->
+ <select style="width:145; font-size:12px;" name = <?php echo "kzlPil_$Id"; ?> >
+  <option></option>
+<?php   $count = count($pilln);
+for ($i = 0; $i < $count; $i++){
+
+    $opties = array($pilId[$i]=>$pilln[$i]);
+            foreach($opties as $key => $waarde)
+            {
+  if ((!isset($_POST['knpVervers_']) && $artId_rd == $pilRaak[$i]) || (isset($_POST["kzlPil_$Id"]) && $_POST["kzlPil_$Id"] == $key)){
+    echo '<option value="' . $key . '" selected>' . $waarde . '</option>';
+  } else { 
+    echo '<option value="' . $key . '" >' . $waarde . '</option>';  
+  }     
+            }
+}
+?> </select>
+ </td> <!-- EINDE KZLMEDICIJN -->
+
+ <td>
+    <input type = "text" style = "font-size : 10px; text-align : right;" size = 1 name = <?php echo "txtAantal_$Id"; ?>  value = <?php echo $aantal; ?> >
+ </td>
+
+ <td align="center" >
+<?php 
+if(!empty($kzlArt) || isset($_POST['knpVervers_'])) {echo $stdat;} ?>
+ </td>
+ <td>
+<?php if(!empty($kzlArt) || isset($_POST['knpVervers_'])) { echo $eenheid; } ?>
+ </td>
+<?php 
+if(empty($reduId)) { $kzlRedu = 'NULL'; } else { $kzlRedu = $reduId; } /*echo '$kzlRedu = '.$kzlRedu;*/
+$zoek_aantal_reden = mysqli_query($db,"
+SELECT count(reduId) aant 
+FROM (
+    SELECT ru.reduId 
+    FROM tblRedenuser ru
+    WHERE ru.lidId = '".mysqli_real_escape_string($db,$lidId)."' and ru.pil = 1
+   union
+    SELECT ru.reduId
+    FROM tblRedenuser ru
+    WHERE ru.lidId = '".mysqli_real_escape_string($db,$lidId)."' and ru.reduId = '".mysqli_real_escape_string($db,$kzlRedu)."'
+ ) B 
+") or die (mysqli_error($db)); 
+    while ($red = mysqli_fetch_array($zoek_aantal_reden)) { $records_klzReden = $red['aant'];   }  
+    
+// Declaratie REDEN (De redenen waaruit kan worden gekozen)
+$queryReden = ("
+SELECT reduId, reden 
+FROM (
+    SELECT ru.reduId, r.reden  
+    FROM tblReden r
+     join tblRedenuser ru on (r.redId = ru.redId)
+    WHERE ru.lidId = '".mysqli_real_escape_string($db,$lidId)."' and ru.pil = 1
+   union
+    SELECT ru.reduId, r.reden
+    FROM tblReden r
+     join tblRedenuser ru on (r.redId = ru.redId)
+    WHERE ru.lidId = '".mysqli_real_escape_string($db,$lidId)."' and ru.reduId = '".mysqli_real_escape_string($db,$kzlRedu)."'
+ ) A
+GROUP BY reduId, reden
+ORDER BY reden
+              "); 
+$qryReden = mysqli_query($db,$queryReden) or die (mysqli_error($db)); 
+
+
+$index = 0; 
+while ($red = mysqli_fetch_array($qryReden)) 
+{ 
+   $rduId[$index] = $red['reduId'];
+   $redn[$index] = $red['reden'];
+   $index++; 
+}
+unset($index); 
+//dan het volgende: 
+// EINDE Declaratie REDEN
+?>
+ <td>
+<!-- KZLREDEN -->
+ <select style="width:145; font-size:12px;" name = <?php echo "kzlReden_$Id"; ?> >
+  <option></option>
+<?php   $count = $records_klzReden;
+for ($i = 0; $i < $count; $i++){
+
+    $opties = array($rduId[$i]=>$redn[$i]);
+            foreach($opties as $key => $waarde)
+            {
+  if ((!isset($_POST['knpVervers_']) && $reduId == $rduId[$i]) || (isset($_POST["kzlReden_$Id"]) && $_POST["kzlReden_$Id"] == $key)){
+    echo '<option value="' . $key . '" selected>' . $waarde . '</option>';
+  } else { 
+    echo '<option value="' . $key . '" >' . $waarde . '</option>';  
+  }     
+            }
+} 
+?> </select>
+ </td> <!-- EINDE KZLREDEN -->
+    
+    
+    
+    
+ <td style = "color : <?php echo $color; ?> ; font-size:12px; " > <?php 
+
+ if (isset($onjuist)) { echo '&nbsp&nbsp '.$onjuist; } ?>
+
  
+ </td>
 
 </tr>
 <!--    **************************************
